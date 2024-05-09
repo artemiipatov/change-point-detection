ff65007b129ada048a8deea51c38d2a9709eb8bc
artemiipatov <patov.988@gmail.com>
2024-05-09 20:22:04 +0300

    add: example

--- src/main.py
+++ src/main.py
@@ -17,23 +17,21 @@ def metric(obs1: obs.Observation, obs2: obs.Observation) -> float:
 START_YEAR = 1851
 
 series = pd.read_excel('../datasets/COAL MINING DISASTERS UK.xlsx')['Count'].to_numpy().astype(np.float64)
-model = cpd.KNNCPD(15, metric)
+model = cpd.KNNCPD(10, metric, k=3)
 statistics_list = []
 
 for observation in series:
     model.update(np.array([observation]))
     statistics_list.append(model.statistics)
-    if len(statistics_list) == 20:
-        print(model._knngraph._window)
 
 print(statistics_list)
-#
-# years = np.arange(START_YEAR, START_YEAR + series.size + 1, 1)
-#
-# plt.plot(years, statistics_list)
-#
-# plt.title('Статистика')
-# plt.xlabel('Год')
-# plt.ylabel('Статистика')
-#
-# plt.show()
+
+years = [i for i in range(START_YEAR, START_YEAR + len(statistics_list))]
+
+plt.plot(years, statistics_list)
+
+plt.title('Статистика')
+plt.xlabel('Год')
+plt.ylabel('Статистика')
+
+plt.show()
